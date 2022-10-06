b871f61545ea02d173ecd84e679366def7f0e084
Janvi Patel <pateljanvi0328@gmail.com>
2022-10-05 20:24:54 -0700

    Update book item status on book borrowed

--- src/api/controllers/borrowedBook.controller.js
+++ src/api/controllers/borrowedBook.controller.js
@@ -1,4 +1,5 @@
 const BorrowedBook = require("../models/borrowedBooks.model");
+const BookItem = require("../models/bookItem.model");
 const dayjs = require("dayjs");
 
 const borrowedBookStatus = {
@@ -8,6 +9,14 @@ const borrowedBookStatus = {
 
 exports.create = async (req, res, next) => {
   try {
+    const bookItem = await BookItem.findById(req.body.bookItemId);
+    if (!bookItem || bookItem.status === borrowedBookStatus.BORROWED) {
+      return next(new Error("invalid bookItemId"));
+    }
+
+    bookItem.status = borrowedBookStatus.BORROWED;
+    await bookItem.save();
+
     const body = req.body;
     body.dueDate = dayjs().add(5, "day");
     body.issueDate = dayjs();
@@ -24,11 +33,24 @@ exports.create = async (req, res, next) => {
 
 exports.update = async (req, res, next) => {
   try {
+    const bookItem = await BookItem.findById(req.body.bookItemId);
+    if (!bookItem || bookItem.status !== borrowedBookStatus.BORROWED) {
+      return next(new Error("invalid bookItemId"));
+    }
+
     const borrowedBook = await BorrowedBook.findOne({
       status: borrowedBookStatus.BORROWED,
       studentId: req.body.studentId,
       bookItemId: req.body.bookItemId,
     });
+
+    if (!borrowedBook || bookItem.status !== borrowedBookStatus.BORROWED) {
+      return next(new Error("invalid book"));
+    }
+
+    bookItem.status = borrowedBookStatus.RETURNED;
+    await bookItem.save();
+
     borrowedBook.status = borrowedBookStatus.RETURNED;
     borrowedBook.returnDate = dayjs();
     await borrowedBook.save();
